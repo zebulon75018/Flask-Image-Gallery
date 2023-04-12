88e9efeec3fae8da1ca24729ba01b4e317a1bc16
cv <zebulon75018@yanhoo.fr>
2023-04-13 01:42:44 +0200

    first commit

--- app.py
+++ app.py
@@ -9,9 +9,19 @@ import sys
 import binascii
 import argparse
 
+NB_IMAGES_PER_REQUEST = 5000
 
 app = Flask("Flask Image Gallery")
 app.config['IMAGE_EXTS'] = [".png", ".jpg", ".jpeg", ".gif", ".tiff"]
+
+parser = argparse.ArgumentParser('Usage: %prog [options]')
+parser.add_argument('root_dir', help='Gallery root directory path')
+parser.add_argument('-l', '--listen', dest='host', default='127.0.0.1', \
+                                    help='address to listen on [127.0.0.1]')
+parser.add_argument('-p', '--port', metavar='PORT', dest='port', type=int, \
+                                default=5000, help='port to listen on [5000]')
+args = parser.parse_args()
+app.config['ROOT_DIR'] = args.root_dir
 
 
 def encode(x):
@@ -20,16 +30,20 @@ def encode(x):
 def decode(x):
     return binascii.unhexlify(x.encode('utf-8')).decode()
 
-
+image_paths = []
+for root,dirs,files in os.walk(app.config['ROOT_DIR']):
+     for file in files:
+        if any(file.endswith(ext) for ext in app.config['IMAGE_EXTS']):
+             image_paths.append(encode(os.path.join(root,file)))
+ 
 @app.route('/')
 def home():
-    root_dir = app.config['ROOT_DIR']
-    image_paths = []
-    for root,dirs,files in os.walk(root_dir):
-        for file in files:
-            if any(file.endswith(ext) for ext in app.config['IMAGE_EXTS']):
-                image_paths.append(encode(os.path.join(root,file)))
-    return render_template('index.html', paths=image_paths)
+    if request.args.get('step') is None:
+         step = 0
+    else: 
+         step = int(request.args.get('step'))
+    print(image_paths[step*NB_IMAGES_PER_REQUEST:NB_IMAGES_PER_REQUEST])
+    return render_template('index.html', paths=image_paths[step*NB_IMAGES_PER_REQUEST:NB_IMAGES_PER_REQUEST*(step+1)], ntotal=len(image_paths),step=step)
 
 
 @app.route('/cdn/<path:filepath>')
@@ -39,12 +53,4 @@ def download_file(filepath):
 
 
 if __name__=="__main__":
-    parser = argparse.ArgumentParser('Usage: %prog [options]')
-    parser.add_argument('root_dir', help='Gallery root directory path')
-    parser.add_argument('-l', '--listen', dest='host', default='127.0.0.1', \
-                                    help='address to listen on [127.0.0.1]')
-    parser.add_argument('-p', '--port', metavar='PORT', dest='port', type=int, \
-                                default=5000, help='port to listen on [5000]')
-    args = parser.parse_args()
-    app.config['ROOT_DIR'] = args.root_dir
-    app.run(host=args.host, port=args.port, debug=True)
+        app.run(host=args.host, port=args.port, debug=True)
